9812e69024695a38cd6432d36d2cb7f23a71cfcf
Pietro Gagliardi <pietro10@mac.com>
2017-08-06 20:33:09 -0400

    Wrote the functionality for album-specific overrides. Not yet implemented.

---
+++ overrides.m
@@ -0,0 +1,26 @@
+// 6 august 2017
+#import "macgetalbums.h"
+
+static const struct {
+	NSString *album;
+	NSString *artist;
+	NSInteger year;
+} overrides[] = {
+	// Each track on this album of previously unreleased material
+	// is dated individually, so our "earliest year" algoritm doesn't
+	// reflect the actual release date.
+	{ @"Lioness: Hidden Treasures", @"Amy Winehouse", 2011 },
+
+	{ nil, nil, 0 },
+};
+
+NSInteger handleOverrides(NSString *album, NSString *artist, NSInteger year)
+{
+	int i;
+
+	for (i = 0; overrides[i].album != nil; i++)
+		if ([album isEqual:overrides[i].album] &&
+			[artist isEqual:overrides[i].artist])
+			return overrides[i].year;
+	return year;
+}
