fa3bb71113c264c326df0ab600bf88f6de5f8443
Pietro Gagliardi <pietro10@mac.com>
2017-08-22 15:16:05 -0400

    Added sorting by length to prepare.sh.

--- prepare.sh
+++ prepare.sh
@@ -1,1 +1,26 @@
-sort -t$'\t' -k1,1n -k2,2 -k3,3 | column -t -s$'\t'
+#!/bin/bash
+
+fourthGen() {
+	awk '{
+		x = $NF
+		gsub(/:/, "", x)
+		print $0 "\t" x
+	}'
+}
+fourthStrip() {
+	cut -d$'\t' -f-4
+}
+
+fourthFirst=cat
+fourthSort=
+fourthLast=cat
+[ z$1 = z-l ] && {
+	fourthFirst=fourthGen
+	fourthSort=-k5,5n
+	fourthLast=fourthStrip
+}
+
+$fourthFirst |
+	sort -t$'\t' $fourthSort -k1,1n -k2,2 -k3,3 |
+	$fourthLast |
+	column -t -s$'\t'
