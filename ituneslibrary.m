97b07dce2fa1897e7911dca92dceda24d2e4db04
Pietro Gagliardi <pietro10@mac.com>
2017-09-03 21:32:02 -0400

    Made the iTunesLibrary.framework code dynamically loaded and with no build-time dependencies.

--- ituneslibrary.m
+++ ituneslibrary.m
@@ -1,20 +1,62 @@
 // 3 september 2017
 #import <Foundation/Foundation.h>
-#import <iTunesLibrary/iTunesLibrary.h>
+
+// To avoid a build-time dependency on iTunesLibrary.framework, recreate the relevant functionality with protocols so we don't have to include the real headers.
+// Thanks to dexter0 in irc.freenode.net/#macdev.
+@protocol ourITLibArtist<NSObject>
+- (NSString *)name;
+@end
+
+@protocol ourITLibAlbum<NSObject>
+- (NSString *)title;
+- (NSString *)albumArtist;
+@end
+
+@protocol ourITLibMediaItem<NSObject>
+- (NSString *)title;
+- (id<ourITLibArtist>)artist;
+- (id<ourITLibAlbum>)album;
+- (NSUInteger)totalTime;
+- (NSDate *)releaseDate;
+- (NSUInteger)year;
+@end
+
+@protocol ourITLibrary<NSObject>
+- (instancetype)initWithAPIVersion:(NSString *)version error:(NSError **)err;
+- (NSArray *)allMediaItems;
+@end
 
 int main(void)
 {
-	ITLibrary *library;
+	NSBundle *framework;
+	Class libraryClass;
+	id<ourITLibrary> library;
 	NSError *err;
 
-	library = [[ITLibrary alloc] initWithAPIVersion:@"1.0" error:&err];
+	framework = [[NSBundle alloc] initWithPath:@"/Library/Frameworks/iTunesLibrary.framework"];
+	if (framework == nil) {
+		NSLog(@"failed to create NSBundle for iTunesLibrary.framework");
+		return 1;
+	}
+	if ([framework loadAndReturnError:&err] == NO) {
+		NSLog(@"failed to load iTunesLibrary.framework: %@", err);
+		return 1;
+	}
+	libraryClass = [framework classNamed:@"ITLibrary"];
+	if (libraryClass == nil) {
+		NSLog(@"failed to load ITLibrary class");
+		return 1;
+	}
+
+	library = (id<ourITLibrary>) [libraryClass alloc];
+	library = [library initWithAPIVersion:@"1.0" error:&err];
 	if (library == nil) {
 		NSLog(@"error initializing library: %@", err);
 		return 1;
 	}
 	NSLog(@"count: %lu", (unsigned long) [[library allMediaItems] count]);
 	// TODO does this only cover music or not? compare to the ScriptingBridge code
-	for (ITLibMediaItem *track in [library allMediaItems]) {
+	for (id<ourITLibMediaItem> track in [library allMediaItems]) {
 		NSLog(@"%@ | %@ | %@ | %@ | %lu(%@) | %lu",
 			[track title],
 			[track artist],
@@ -25,5 +67,9 @@ int main(void)
 			(unsigned long) [track totalTime]);
 	}
 	[library release];
+
+	if ([framework unload] == NO)
+		NSLog(@"warning: failed to unload iTunesLibrary.framework");
+	[framework release];
 	return 0;
 }
