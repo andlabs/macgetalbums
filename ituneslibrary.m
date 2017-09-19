4d37d2eb155f085514494ec707f6ab6df7dbc631
Pietro Gagliardi <pietro10@mac.com>
2017-09-18 23:42:41 -0400

    Finished the rewrite of ituneslibrary.m. Now to integrate it.

--- ituneslibrary.m
+++ ituneslibrary.m
@@ -26,8 +26,14 @@
 - (NSArray *)allMediaItems;
 @end
 
+// TODO should this be autoreleased?
+#define genericError() [[NSError alloc] initWithDomain:NSMachErrorDomain code:KERN_FAILURE userInfo:nil]
+
 // TODO figure out how far back we can have ivars in @implementation
 @implementation iTunesLibraryCollector {
+	Timer *timer;
+	NSBundle *framework;
+	id<ourITLibrary> library;
 }
 
 + (NSString *)collectorName
@@ -37,10 +43,6 @@
 
 + (BOOL)canRun
 {
-	// TODO
-	return NO;
-
-	// TODO instead of calling this, test it at startup
 	return amISigned;
 }
 
@@ -48,67 +50,88 @@
 {
 	self = [super init];
 	if (self) {
-		// TODO
+		// technically this should be Nil, but the iTunesLibrary.framework documentation uses nil, so eh
+		Class libraryClass = nil;
+
+		self->timer = t;
+		[self->timer retain];
+
+		// initialize everything to be safe
+		self->framework = nil;
+		self->library = nil;
+		*err = nil;
+
+		[self->timer start:TimerLoad];
+		self->framework = [[NSBundle alloc] initWithPath:@"/Library/Frameworks/iTunesLibrary.framework"];
+		if (self->framework == nil) {
+			*err = genericError();
+			goto out;
+		}
+		if ([self->framework loadAndReturnError:err] == NO) {
+			// Apple's docs are self-contradictory as to whether err is non-nil here.
+			if (*err == nil)
+				*err = genericError();
+			goto out;
+		}
+		libraryClass = [self->framework classNamed:@"ITLibrary"];
+		if (libraryClass == nil) {
+			// TODO find a class not found error?
+			*err = genericError();
+			goto out;
+		}
+		// TODO is this really collection...?
+		self->library = (id<ourITLibrary>) [libraryClass alloc];
+		self->library = [self->library initWithAPIVersion:@"1.0" error:err];
+		if (self->library == nil)
+			// Apple's docs say that err *will* be filled.
+			goto out;
+	out:
+		[self->timer end];
 	}
 	return self;
 }
 
 - (void)dealloc
 {
-	// TODO
+	if (self->library != nil)
+		[self->library release];
+	if (self->framework != nil) {
+		// ignore any error; there's not much we can do if this fails anyway
+		[self->framework unload];
+		[self->framework release];
+	}
+	[self->timer release];
 	[super dealloc];
 }
 
 // TODO make this an instance stuff
 - (NSArray *)collectTracks
 {
-abort();return nil;
-#if 0//TODO
-	NSBundle *framework;
-	Class libraryClass;
-	id<ourITLibrary> library;
-	NSError *err;
+	NSArray *tracks;
+	NSMutableArray *items;
 
-	framework = [[NSBundle alloc] initWithPath:@"/Library/Frameworks/iTunesLibrary.framework"];
-	if (framework == nil) {
-		NSLog(@"failed to create NSBundle for iTunesLibrary.framework");
-		return 1;
-	}
-	if ([framework loadAndReturnError:&err] == NO) {
-		NSLog(@"failed to load iTunesLibrary.framework: %@", err);
-		return 1;
-	}
-	libraryClass = [framework classNamed:@"ITLibrary"];
-	if (libraryClass == nil) {
-		NSLog(@"failed to load ITLibrary class");
-		return 1;
-	}
+	[self->timer start:TimerCollect];
+	tracks = [self->library allMediaItems];
+	[self->timer end];
 
-	library = (id<ourITLibrary>) [libraryClass alloc];
-	library = [library initWithAPIVersion:@"1.0" error:&err];
-	if (library == nil) {
-		NSLog(@"error initializing library: %@", err);
-		return 1;
-	}
-	NSLog(@"count: %lu", (unsigned long) [[library allMediaItems] count]);
+	// TODO add a TimerConvert
+	items = [[NSMutableArray alloc] initWithCapacity:[tracks count]];
 	// TODO does this only cover music or not? compare to the ScriptingBridge code
-	for (id<ourITLibMediaItem> track in [library allMediaItems]) {
-		NSLog(@"%@ | %@ | %@ | %@ | %lu(%@) | %lu",
-			[track title],
-			[track artist],
-			[[track album] title],
-			[[track album] albumArtist],
-			(unsigned long) [track year],
-			[track releaseDate],
-			(unsigned long) [track totalTime]);
-	}
-	[library release];
+	for (id<ourITLibMediaItem> track in tracks) {
+		Item *item;
 
-	if ([framework unload] == NO)
-		NSLog(@"warning: failed to unload iTunesLibrary.framework");
-	[framework release];
-	return nil;
-#endif
+		item = [[Item alloc] initWithYear:[track year]
+			trackArtist:[[track artist] name]
+			album:[[track album] title]
+			albumArtist:[[track album] albumArtist]
+			lengthMilliseconds:[track totalTime]];
+		[items addObject:item];
+		[item release];		// and release the initial reference
+	}
+
+	// TODO is this release correct?
+	[tracks release];
+	return items;
 }
 
 @end
